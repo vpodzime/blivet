52b8a2912687ea78212f5dd2ed4557b7408817fa
Vratislav Podzimek <vpodzime@redhat.com>
2016-01-25 15:47:51 +0100

    fixes for is_valid_name

--- tests/devices_test/device_names_test.py
+++ tests/devices_test/device_names_test.py
@@ -5,9 +5,10 @@ import unittest
 from blivet.devices import LVMVolumeGroupDevice
 from blivet.devices import LVMLogicalVolumeDevice
 from blivet.devices import StorageDevice
+from blivet.size import Size
+import blivet
 
 
-@unittest.skip("Skipping is_name_valid tests")
 class DeviceNameTestCase(unittest.TestCase):
 
     """Test device name validation"""
@@ -17,29 +18,41 @@ class DeviceNameTestCase(unittest.TestCase):
         good_names = ['sda1', '1sda', 'good-name', 'cciss/c0d0']
         bad_names = ['sda/1', 'sda\x00', '.', '..', 'cciss/..']
 
+        sd = StorageDevice("tester")
+
         for name in good_names:
-            self.assertTrue(StorageDevice.is_name_valid(name))
+            self.assertTrue(sd.is_name_valid(name))
 
         for name in bad_names:
-            self.assertFalse(StorageDevice.is_name_valid(name))
+            self.assertFalse(sd.is_name_valid(name))
 
     def test_volume_group(self):
         good_names = ['vg00', 'group-name', 'groupname-']
         bad_names = ['-leading-hyphen', 'únicode', 'sp aces']
 
+        pv = StorageDevice("pv1", fmt=blivet.formats.get_format("lvmpv"),
+                           size=Size("1 GiB"))
+        vg = LVMVolumeGroupDevice("testvg", parents=[pv])
+
         for name in good_names:
-            self.assertTrue(LVMVolumeGroupDevice.is_name_valid(name))
+            self.assertTrue(vg.is_name_valid(name))
 
         for name in bad_names:
-            self.assertFalse(LVMVolumeGroupDevice.is_name_valid(name))
+            self.assertFalse(vg.is_name_valid(name))
 
     def test_logical_volume(self):
         good_names = ['lv00', 'volume-name', 'volumename-']
         bad_names = ['-leading-hyphen', 'únicode', 'sp aces',
                      'snapshot47', 'pvmove0', 'sub_tmetastring']
 
+        pv = StorageDevice("pv1", fmt=blivet.formats.get_format("lvmpv"),
+                           size=Size("1 GiB"))
+        vg = LVMVolumeGroupDevice("testvg", parents=[pv])
+        lv = LVMLogicalVolumeDevice("testlv", parents=[vg],
+                                    fmt=blivet.formats.get_format("xfs"))
+
         for name in good_names:
-            self.assertTrue(LVMLogicalVolumeDevice.is_name_valid(name))
+            self.assertTrue(lv.is_name_valid(name))
 
         for name in bad_names:
-            self.assertFalse(LVMLogicalVolumeDevice.is_name_valid(name))
+            self.assertFalse(lv.is_name_valid(name))
