76125a481d8bd97e879e7a41466b265d0a0a47c4
Vratislav Podzimek <vpodzime@redhat.com>
2016-02-08 14:42:14 +0100

    Update the minimum device size for btrfs
    It seems to be 128 MiB now. See the libblockdev's commit [1] for more details.
    
    [1] https://github.com/rhinstaller/libblockdev/commit/484179732bf453a1cc74ee5456e99b98f2f32d25

--- blivet/devicelibs/btrfs.py
+++ blivet/devicelibs/btrfs.py
@@ -31,7 +31,7 @@ log = logging.getLogger("blivet")
 MAIN_VOLUME_ID = 5
 
 # if any component device is less than this size, mkfs.btrfs will fail
-MIN_MEMBER_SIZE = Size("16 MiB")
+MIN_MEMBER_SIZE = Size("128 MiB")
 
 raid_levels = raid.RAIDLevels(["raid0", "raid1", "raid10", "single"])
 
